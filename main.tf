3dd1026ac5f88674d459ec32872533e9ddc7eb2a
nikita.vinogradov <vinni.ru@gmail.com>
2021-11-12 01:19:09 +0300

    HW 7.4 pr_3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "$AWS_ACCESS_KEY_ID"
-  secret_key = "$AWS_SECRET_ACCESS_KEY"
+#  access_key = "$AWS_ACCESS_KEY_ID"
+#  secret_key = "$AWS_SECRET_ACCESS_KEY"
   region = "us-west-2"
 }
 
